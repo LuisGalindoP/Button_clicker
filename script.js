783955807f9b74c42dadf5ac5015e0190570bfcc
luis galindo <luisfgp80@gmail.com>
2021-10-12 16:14:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function textChange() {
+    console.log("test");
+    document.getElementById("login").innerHTML="Logout"; 
+}
+
+function removeButton() {
+    document.getElementById("AddDefinition").style.visibility="hidden";
+}
+function addLike() {
+    console.log("oine like");
+    alert("Ninja was liked");
+}
